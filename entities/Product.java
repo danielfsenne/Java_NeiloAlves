cb872c4d793dd4074c58b9b1fc9be2c46ba91e55
danielSenne <danielflausinosenne123@gmail.com>
2026-06-07 12:04:11 -0300

    uso do encapsulamento

--- entities/Product.java
+++ entities/Product.java
@@ -1,9 +1,9 @@
 package entities;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product(String name, double price, int quantity){
         this.name = name;
@@ -16,6 +16,25 @@ public class Product {
         this.price = price;
     }
 
+    public String getName(){
+        return  name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(){
+        this.price = price;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
 
     public double totalValueStock(){
         return price * quantity;
