3ac073d946e774b80bdc13bba8d5399190b4a5fa
danielSenne <danielflausinosenne123@gmail.com>
2026-06-07 11:37:40 -0300

    uso de construtores

--- application/SegundoProblema.java
+++ application/SegundoProblema.java
@@ -10,14 +10,15 @@ public class SegundoProblema {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
         System.out.println("Enter product data:");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantity in Stock: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
 
         System.out.println();
         System.out.println("Product data:" + product);
