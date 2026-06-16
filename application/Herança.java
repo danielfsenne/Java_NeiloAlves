fb40fd410cbf93846ea41ad632087dfb08846d2f
danielSenne <danielflausinosenne123@gmail.com>
2026-06-16 08:18:12 -0300

    associação entre classes (Herança).

---
+++ application/Herança.java
@@ -0,0 +1,10 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Herança {
+   public static void main(String[] args){
+       BusinessAccount acount = new BusinessAccount();
+       acount.getBalance();
+   }
+}
---
+++ entities/Acount.java
@@ -0,0 +1,46 @@
+package entities;
+
+public class Acount {
+    private Integer number;
+    private String holder;
+    private Double balance;
+
+    public Acount() {
+    }
+
+    public Acount(Integer number, String holder, Double balance) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    // Getter e Setter para number
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    // Getter e Setter para holder
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+}
