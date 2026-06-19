143ca32ecec593ae2954ea5e2fe11878d924dbc7
danielSenne <danielflausinosenne123@gmail.com>
2026-06-19 08:57:21 -0300

    stack trace

---
+++ application/PilhasDeChamadas.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PilhasDeChamadas {
+    public static void main(String[]args){
+        method1();
+        System.out.println("end program");
+    }
+
+    public static void method1(){
+        System.out.println("***METHOD2 START****");
+        method2();
+        System.out.println("***METHOD2 END****");
+
+    }
+
+    public static void method2(){
+        System.out.println("***METHOD2 START****");
+        Scanner sc = new Scanner(System.in);
+
+
+        try {
+            String [] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid position");
+        }
+        catch (InputMismatchException e){
+            System.out.println("Input error");
+            e.printStackTrace();
+            sc.next();
+        }
+
+        sc.close();
+    }
+}
