a04d94a5e3b1a20ca797d41dc5d35303e1180006
danielSenne <danielflausinosenne123@gmail.com>
2026-07-01 11:27:46 -0300

    uso do Function

--- application/Predicate.java
+++ application/Predicate.java
@@ -3,9 +3,11 @@ package application;
 import entities.Product;
 import util.PriceUpdate;
 import util.ProductPredicate;
+import util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Predicate {
 
@@ -17,6 +19,12 @@ public class Predicate {
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 80.00));
 
+
+        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+
+        names.forEach(System.out::println);
+
+        /*
         list.forEach(new PriceUpdate());
 
         list.forEach(System.out::println);
