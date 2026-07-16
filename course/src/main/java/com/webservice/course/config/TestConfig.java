eb5f61676d083967e20e00b7fb8dbebca4df2157
danielSenne <danielflausinosenne123@gmail.com>
2026-07-16 11:13:48 -0300

    jpa repository e injecao de dependencia

---
+++ course/src/main/java/com/webservice/course/config/TestConfig.java
@@ -0,0 +1,26 @@
+package com.webservice.course.config;
+
+import com.webservice.course.entities.User;
+import com.webservice.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "João Brown", "joao@gmail.com", "98777777", "123457");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
