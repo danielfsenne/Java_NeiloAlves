ae88ac2c39286e9bf4cd6aa87574c6ae2573e9d1
danielSenne <danielflausinosenne123@gmail.com>
2026-07-20 08:42:24 -0300

    tratamento de exceção, findById

--- course/src/main/java/com/webservice/course/service/UserService.java
+++ course/src/main/java/com/webservice/course/service/UserService.java
@@ -2,6 +2,7 @@ package com.webservice.course.service;
 
 import com.webservice.course.entities.User;
 import com.webservice.course.repositories.UserRepository;
+import com.webservice.course.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
