10ce597493d31c5bbba398e424875b5e1cb45cfd
danielSenne <danielflausinosenne123@gmail.com>
2026-07-20 08:17:41 -0300

    user delete

--- course/src/main/java/com/webservice/course/service/UserService.java
+++ course/src/main/java/com/webservice/course/service/UserService.java
@@ -27,4 +27,9 @@ public class UserService {
     public User insert(User obj){
         return repository.save(obj);
     }
+
+    public User delete(Long id){
+        repository.deleteById(id);
+        return null;
+    }
 }
