38f756ddf2c94c172c99f777fea80fc09c1b43ff
danielSenne <danielflausinosenne123@gmail.com>
2026-08-02 12:02:41 -0300

    inserção de usuário com POST

--- demo/demo/src/main/java/com/workshop/demo/services/UserService.java
+++ demo/demo/src/main/java/com/workshop/demo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.workshop.demo.services;
 
 import com.workshop.demo.domain.User;
+import com.workshop.demo.dto.UserDTO;
 import com.workshop.demo.repository.UserRepository;
 import com.workshop.demo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
