914c2ca54cc3db94927558d1a8453184e4a1191e
danielSenne <danielflausinosenne123@gmail.com>
2026-08-04 10:32:49 -0300

    consulta simples com query methods

--- demo/demo/src/main/java/com/workshop/demo/resources/PostResource.java
+++ demo/demo/src/main/java/com/workshop/demo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.workshop.demo.resources;
 import com.workshop.demo.domain.Post;
 import com.workshop.demo.domain.User;
 import com.workshop.demo.dto.UserDTO;
+import com.workshop.demo.resources.util.URL;
 import com.workshop.demo.services.PostService;
 import com.workshop.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +26,12 @@ public class PostResource {
     public ResponseEntity<Post> findPosts(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value ="/title-search", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
