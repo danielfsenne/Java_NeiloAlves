3f8b93c2b45f40357951bf42410bdf2f8eacf615
danielSenne <danielflausinosenne123@gmail.com>
2026-08-04 08:36:57 -0300

    endpoint para retornar os posts de um usuário

--- demo/demo/src/main/java/com/workshop/demo/resources/UserResource.java
+++ demo/demo/src/main/java/com/workshop/demo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.workshop.demo.resources;
 
+import com.workshop.demo.domain.Post;
 import com.workshop.demo.domain.User;
 import com.workshop.demo.dto.UserDTO;
 import com.workshop.demo.services.UserService;
@@ -48,4 +49,9 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
+    }
 }
