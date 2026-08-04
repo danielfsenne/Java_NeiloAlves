4a51b8adf8b56dde137b02ea30a681d3a20b701c
danielSenne <danielflausinosenne123@gmail.com>
2026-08-04 08:45:09 -0300

    obtendo um post por id

---
+++ demo/demo/src/main/java/com/workshop/demo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.workshop.demo.resources;
+
+import com.workshop.demo.domain.Post;
+import com.workshop.demo.domain.User;
+import com.workshop.demo.dto.UserDTO;
+import com.workshop.demo.services.PostService;
+import com.workshop.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findPosts(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ demo/demo/src/main/java/com/workshop/demo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.workshop.demo.services;
+
+import com.workshop.demo.domain.Post;
+import com.workshop.demo.repository.PostRepository;
+import com.workshop.demo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> post = repo.findById(id);
+        return post.orElseThrow(() ->
+                new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
