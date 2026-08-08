aab9a443a34a5bf5c30523ce239b695b2b29ac95
danielSenne <danielflausinosenne123@gmail.com>
2026-08-08 14:19:39 -0300

    limitações para o textField, interface initialize

--- javaFX/src/main/java/controllers/ViewController.java
+++ javaFX/src/main/java/controllers/ViewController.java
@@ -1,15 +1,19 @@
 package controllers;
 
 import gui.util.Alerts;
+import gui.util.Constrains;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class ViewController {
+public class ViewController implements Initializable {
 
     @FXML
     private TextField txtNumber1;
@@ -33,4 +37,11 @@ public class ViewController {
         labelResult.setText(String.format("%.2f", sum));
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Constrains.setTextFieldDouble(txtNumber1);
+        Constrains.setTextFieldDouble(txtNumber2);
+        Constrains.setTextFieldMaxLength(txtNumber1,12);
+        Constrains.setTextFieldMaxLength(txtNumber2,12);
+    }
 }
