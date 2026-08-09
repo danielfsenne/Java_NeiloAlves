74b5b481f981d078503b05544eb9f8c3fa1ab017
danielSenne <danielflausinosenne123@gmail.com>
2026-08-09 12:33:26 -0300

    projeto com jdbc e javafx

---
+++ workshop-javafx-jdbc/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Workshop JavaFX JDBC");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
