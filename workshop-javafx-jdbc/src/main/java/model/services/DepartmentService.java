7b5567ce04e6bda5aeb04b871cbd682e194f4f20
danielSenne <danielflausinosenne123@gmail.com>
2026-08-10 21:34:09 -0300

    DepartmentForm (dialog) design

--- workshop-javafx-jdbc/src/main/java/model/services/DepartmentService.java
+++ workshop-javafx-jdbc/src/main/java/model/services/DepartmentService.java
@@ -4,12 +4,11 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentService {
 
-    private DepartmentDao dao = DaoFactory.createSellerDao();
+    private DepartmentDao dao = DaoFactory.createDepartmentDao();
 
     public List<Department> findAll() {
         return dao.findAll();
