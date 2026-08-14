cb1abe88e98de116182783a53dde8274fbb08774
danielSenne <danielflausinosenne123@gmail.com>
2026-08-14 11:50:36 -0300

    passando um objeto Department para o fromulário

--- workshop-javafx-jdbc/src/main/java/controller/DepartmentFormController.java
+++ workshop-javafx-jdbc/src/main/java/controller/DepartmentFormController.java
@@ -6,11 +6,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DepartmentFormController implements Initializable {
+
+    private Department entity;
 
     @FXML
     private TextField txtId;
@@ -23,6 +26,11 @@ public class DepartmentFormController implements Initializable {
 
     @FXML
     private Button btSave;
+
+    public void setDepartment(Department entity) {
+        this.entity = entity;
+    }
+
 
     @FXML
     public void onBtSaveAction() {
@@ -41,6 +49,14 @@ public class DepartmentFormController implements Initializable {
     private void initializeNodes() {
         Constraints.setTextFieldInteger(new TextField());
         Constraints.setTextFieldMaxLength(txtName, 30);
+    }
+
+    public void updateFormData() {
+        if (entity == null) {
+            throw new IllegalStateException("Entity was null");
+        }
+        txtId.setText(String.valueOf(entity.getId()));
+        txtName.setText(entity.getName());
     }
 
 }
