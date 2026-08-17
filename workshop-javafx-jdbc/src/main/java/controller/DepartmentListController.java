9b79d4ba2db4e5815a24b7c787dbead0886f05a3
danielSenne <danielflausinosenne123@gmail.com>
2026-08-17 08:38:47 -0300

    padrão de projeto Observer para atualizar o TablrView

--- workshop-javafx-jdbc/src/main/java/controller/DepartmentListController.java
+++ workshop-javafx-jdbc/src/main/java/controller/DepartmentListController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -26,7 +27,7 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class DepartmentListController implements Initializable {
+public class DepartmentListController implements Initializable, DataChangeListener {
 
     private DepartmentService service;
 
@@ -86,6 +87,7 @@ public class DepartmentListController implements Initializable {
             DepartmentFormController controller = loader.getController();
             controller.setDepartment(obj);
             controller.setDepartmentService(new DepartmentService());
+            controller.subscribeDataChangeListener(this);
             controller.updateFormData();
 
             Stage dialogStage = new Stage();
@@ -99,5 +101,10 @@ public class DepartmentListController implements Initializable {
         catch (IOException e) {
             Alerts.showAlerts("IOException", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
+    }
+
+    @Override
+    public void onDataChange() {
+        updateTableView();
     }
 }
