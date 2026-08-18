f904bc3719cf7716c871dc761f0f7dc4bf5388f1
danielSenne <danielflausinosenne123@gmail.com>
2026-08-18 08:33:56 -0300

    SellerList TableView

--- workshop-javafx-jdbc/src/main/java/gui/util/Utils.java
+++ workshop-javafx-jdbc/src/main/java/gui/util/Utils.java
@@ -2,7 +2,14 @@ package gui.util;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Utils {
 
@@ -17,6 +24,46 @@ public class Utils {
         catch (NumberFormatException e)  {
             return null;
         }
+    }
+
+    public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Date> cell = new TableCell<T, Date>() {
+                private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+                @Override
+                protected void updateItem(Date item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        setText(sdf.format(item));
+                    }
+                }
+            };
+
+            return cell;
+        });
+    }
+
+    public static <T> void formatTableColumnDouble(TableColumn<Seller, Double> tableColumn, int decimalPlaces) {
+        tableColumn.setCellFactory(column -> {
+            TableCell<T, Double> cell = new TableCell<T, Double>() {
+
+                @Override
+                protected void updateItem(Double item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setText(null);
+                    } else {
+                        Locale.setDefault(Locale.US);
+                        setText(String.format("%."+decimalPlaces+"f", item));
+                    }
+                }
+            };
+
+            return cell;
+        });
     }
 
 }
